4d47576614b581c170e99c9e588d36644f33ca96
ArpitPundir <thakurarpitpundir73@gmail.com>
2022-03-05 12:56:26 +0530

    Solved top-view

--- topView.cpp
+++ topView.cpp
@@ -1,90 +1,44 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-class node {
-  public:
-  int val;
-  int hd;
-  node *right;
-  node *left;
-  node(int val){
-    this->val = val;
-    this->left = NULL;
-    this->right = NULL;
-  }
-};
-
-node* build(){
-
-  int d;
-cin>>d;
-
-node*root = new node(d);
-queue<node*> q;
-q.push(root);
-
-while(!q.empty()){
-
-	node*f = q.front();
-	q.pop();
-	int c1,c2;
-	cin>>c1>>c2;
-
-	if(c1!=-1){
-		f->left = new node(c1);
-		q.push(f->left);
-	}
-	if(c2!=-1){
-		f->right = new node(c2);
-		q.push(f->right);
-	}
-}
-return root;
-}
-
-void topView(node *root){
-  map<int, int> m;
-  deque<node*> q;
-  root->hd = 0;
-
-  q.push_back(root);
-  q.push_back(NULL);
-
-  while(!q.empty()){
-
-    node* root = q.front();
-    q.pop_front();
-
-    if(root == NULL){
-      if(q.front() == NULL)
-      break;
-      else
-      q.push_back(NULL);
-    }else{
-
-      if(m.count(root->hd) == 0)
-      m[root->hd] = root->val;
-
-
-      if(root->left != NULL){
-        root->left->hd = root->hd-1;
-        q.push_back(root->left);
-      }
-
-      if(root->right != NULL){
-        root->right->hd = root->hd+1;
-        q.push_back(root->right);
-      }
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        vector<int> ans;
+        map<int, Node*> mp;
+        queue<pair<Node*, int> > q;
+        q.push({root, 0}); q.push({NULL, 0});
+        
+        while(!q.empty()){
+             auto curr = q.front();
+            q.pop();
+            
+            Node* currNode = curr.first;
+            int currDist = curr.second;
+            
+            if(currNode == NULL){
+                if(!q.empty()){
+                    q.push({NULL,0});
+                }
+                continue;
+            }
+            
+            if(mp.find(currDist) == mp.end()){
+                mp[currDist] = currNode;
+            }
+            if(currNode->left){
+                q.push({currNode->left, currDist-1});
+            }
+            if(currNode->right){
+                q.push({currNode->right, currDist+1});
+            }
+        }
+        
+        for(auto it: mp){
+            ans.push_back(it.second->data);
+        }
+        return ans;
     }
-  }
-  map<int, int>::iterator i;
-  for(i = m.begin(); i != m.end(); i++){
-    cout<<i->second<<" ";
-  }
-}
 
-int main(){
-  node *root = build();
-  topView(root);
-  return 0;
-}
+};
