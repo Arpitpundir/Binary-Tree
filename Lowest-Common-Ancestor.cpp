6891c2a09d4d562820bd3539f5caa5508e907bbb
Arpitpundir <thakurarpitpundir73@gmail.com>
2022-03-06 21:54:52 +0530

    Solved Lowest Common Ancestor

--- Lowest-Common-Ancestor.cpp
+++ Lowest-Common-Ancestor.cpp
@@ -1,61 +1,39 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-class node{
-  int val;
-  node *left = NULL;
-  node *right = NULL;
+class Solution {
+public:
+    TreeNode* lcaUtil(TreeNode* root, int n1,int n2,bool &b1, bool &b2){
+        if(root==NULL)
+            return NULL;
+        if(root->val==n1){
+            b1=true;
+            return root;
+        }
+        if(root->val==n2){
+            b2=true;
+            return root;
+        }
+        TreeNode* leftAns=lcaUtil(root->left,n1,n2,b1,b2);
+        TreeNode* rightAns=lcaUtil(root->right,n1,n2,b1,b2);
+        if(leftAns&&rightAns)
+            return root;
+        return leftAns==NULL?rightAns:leftAns;
+    }
+    bool findNode(TreeNode* root, int val){
+        if(root==NULL)
+            return false;;
+        if(root->val==val)
+            return true;
+        return findNode(root->left,val)||findNode(root->right,val);
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        bool b1=false,b2=false;
+        TreeNode* lca=lcaUtil(root,p->val,q->val,b1,b2);
+        if(b1&&b2)
+            return lca;
+        if(b1&&findNode(lca, q->val))
+            return lca;
+        if(b2&&findNode(lca,p->val))
+            return lca;
+        return NULL;
+        
+    }
 };
-
-node* findLcaUtil(node *root, bool &isFound1, bool &isFound2, int n1, int n2){
-  if(!root) return NULL;
-
-  if(root->val == n1){
-    isFound1 = true;
-    return root;
-  }
-
-  if(root->val == n2){
-    isFound2 = true;
-    return root;
-  }
-
-  node *leftSubtreeAns = findLcaUtil(root->left, isFound1, isFound2, n1,n2);
-  node *rightSubtreeAns = findLcaUtil(root->right, isFound2, isFound2,n1,n2);
-
-  if(leftSubtreeAns&&rightSubtreeAns) return root;
-  if(leftSubtreeAns){
-    return leftSubtreeAns;
-  }
-  if(rightSubtreeAns){
-    return rightSubtreeAns;
-  }
-
-  return NULL;
-}
-
-bool find(node *root, int val){
-  if(!root) return false;
-  if(root->val == val) return true;
-  return find(root->left,val)||find(root->right, val);
-}
-
-int main(){
-  bool isFound1=false, isFound2 = false;
-
-  node *lca = findLcaUtil(root,isFound1,isFound2,val1,val2);
-  if(isFound1 && isFound2) return lca;
-  else if(isfound1){
-    if(findLcaUtil(lca, val2)){
-      return lca;
-    }else{
-      return NULL;
-    }
-  }else{
-    if(findLcaUtil(lca, val1)){
-      return lca;
-    }else{
-      return NULL;
-    }
-  }
-}
